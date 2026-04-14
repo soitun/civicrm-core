add86198a4b0dcb4bf0ff1634e1aafda4ae24915
Paul Rooney <paulrooney@61725.no-reply.drupal.org>
2026-04-14 16:42:41 -0400

    Fix for issue #6440: Replace null with an empty string.

--- CRM/Dedupe/Merger.php
+++ CRM/Dedupe/Merger.php
@@ -1346,7 +1346,7 @@ INNER JOIN  civicrm_membership membership2 ON membership1.membership_type_id = m
 
       if ($name === 'rel_table_users') {
         // @todo - this user url stuff is only needed for the form layer - move to CRM_Contact_Form_Merge
-        $relTables[$name]['main_url'] = str_replace('%ufid', CRM_Core_BAO_UFMatch::getUFId($mainID), $relTables[$name]['url']);
+        $relTables[$name]['main_url'] = str_replace('%ufid', CRM_Core_BAO_UFMatch::getUFId($mainID) ?? '', $relTables[$name]['url']);
         $relTables[$name]['other_url'] = str_replace('%ufid', CRM_Core_BAO_UFMatch::getUFId($otherID), $relTables[$name]['url']);
       }
       if ($name === 'rel_table_memberships') {
